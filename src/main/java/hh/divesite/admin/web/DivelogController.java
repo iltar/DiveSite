a379744c9443033396f2f8058b457c49cab18d68
Ilta Riikonen <ilta.riikonen@koodikaivos.fi>
2025-11-02 17:03:47 +0200

    DS3.1-3.2 Divelog edit and delete methods

--- src/main/java/hh/divesite/admin/web/DivelogController.java
+++ src/main/java/hh/divesite/admin/web/DivelogController.java
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import hh.divesite.admin.domain.Divelog;
 import hh.divesite.admin.domain.DivelogRepository;
+
+
 
 
 
@@ -33,11 +36,21 @@ public class DivelogController {
         return "createdivelog";
     }
 
+    @GetMapping("/editDivelog/{id}")
+    public String getEditDivelog(@PathVariable() Long id, Model model) {
+        model.addAttribute(rep.findById(id));
+        return "editdivelog";
+    }
+
     @PostMapping("/saveDivelog")
-    public String postMethodName(@ModelAttribute Divelog dl) {
+    public String saveDivelog(@ModelAttribute Divelog dl) {
         rep.save(dl);
         return "redirect:/divelogs";
     }
     
-    
+    @GetMapping("/deleteDivelog/{id}")
+    public String deleteDivelog(@PathVariable() Long id) {
+        rep.deleteById(id);
+        return "redirect:/divelogs";
+    }
 }
